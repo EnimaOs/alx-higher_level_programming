8dab9b537f4f2a1a1097ee9c2f836fa7b1ec6071
EnimaOs <ing.devov@gmail.com>
2023-11-29 00:14:29 +0100

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
-	return ord(c) >= ord('a') and ord(c) <= ord('z')
+    return ord(c) >= ord('a') and ord(c) <= ord('z')
 
 
 def uppercase(str):
-	for c in str:
-		print("{:c}".format(ord(c) - 32 if islower(c) else ord(c)), end='')
-	print()
+    for c in str:
+        print("{:c}".format(ord(c) - 32 if islower(c) else ord(c)), end='')
+    print()
