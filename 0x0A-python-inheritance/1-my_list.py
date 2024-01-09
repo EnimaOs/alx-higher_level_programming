4f8354de32cf73f6e83f80b55e55c570cd0e7397
EnimaOs <ing.devov@gmail.com>
2024-01-09 02:05:23 +0100

    up

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-"""available attributes"""
+'''Module for task 1'''
 
 
 class MyList(list):
-    """A custom list class that inherits from the built-in list class."""
+    '''Subclass of list'''
+
     def print_sorted(self):
-        """
-        Print the list elements in sorted order.
+        '''Print a list in sorted ascending'''
+        print(sorted(self))
 
-        Prints the list in ascending order using the sorted() function.
 
-        Args:
-            None
 
-        Returns:
-            None
-        """
-        my_list = sorted(self)
-        if my_list:
-            print(my_list)
+print(sorted.__dir__())
